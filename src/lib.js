a91e7bdf29f6ae7106bd0da4f9eddcb6b523d320
Richard Marks <ccpsceo@gmail.com>
2014-05-04 21:38:37 -0500

    clay io added to project

---
+++ src/lib.js
@@ -0,0 +1,44 @@
+/*
+ * Clay.IO API Integration
+*/
+var Clay = Clay || {};
+
+Clay.gameKey = "bbasracquetball";
+Clay.readyFunctions = [];
+Clay.options = { debug: true };
+Clay.ready = function( fn ) {
+    Clay.readyFunctions.push(fn);
+};
+
+(function() {
+    var clay = document.createElement("script");
+    
+    clay.async = true;
+    clay.src = ( "https:" == document.location.protocol ? "https://" : "http://" ) + "clay.io/api/api-stats-achievement-twitter-facebook-stream-ratings-screenshot-ads.js"; 
+    
+    var tag = document.getElementsByTagName("script")[0];
+    tag.parentNode.insertBefore(clay, tag);
+})();
+
+var ClayWrap = (function() {
+    return {
+        showAds: function() {
+            new Clay.Advertisement({ size: 'preroll' });
+        },
+        showRatings: function() {
+            new Clay.Ratings();
+        },
+        logStat: function(stat) {
+            Clay.Stats.logStat(stat);
+        },
+        fbPost: function(msg) {
+            var screenshot = new Clay.Screenshot({ prompt: false });
+            screenshot.save(function(response) {
+                Clay.Facebook.post({ message: msg, picture: response.imageSrc });
+            });
+        },
+        award: function(awardId) {
+            (new Clay.Achievement({ id: awardId })).award();
+        }
+    };
+})();
